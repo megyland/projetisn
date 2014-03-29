f3e142f790b2db5269dd0acc602f334dfbc77cf3
Choumat <Choumat@users.noreply.github.com>
2014-03-29 13:56:25 +0100

    Update entity.py

--- entity.py
+++ entity.py
@@ -7,35 +7,36 @@ class Entity:
 		self.y=dy
 		if type == "ennemy":
 			self.d="right"
-			self.v=0.06
-			self.a=0.005
+			self.v=0.1
+			self.a=0.003
 	def move(self) : # gestion de l'IA de Schro.
 		# x : abscisse
 		# y : ordonnée
 		# d : direction actuelle (haut, gauche, droite)
-		# a : accélération (défaut=0.3)
+		# a : accélération
 		# v : vitesse actuelle
-		fv = (1 + self.a) * self.v
+		fv = (1 + self.a ) * self.v
 		fx = self.x
 		fy = self.y
 
 		if self.d == "left" :
 			fx = self.x - fv
-			if fx < 0 : # changement de direction lorsque Schro. arrive en bout de ligne (par la gauche)
-				fx = 0
-				fd = "up"
+			if fx <= 0 : # changement de direction lorsque Schro. arrive en bout de ligne (par la gauche)
+				self.x = 0
+				self.d = "up"
 			else :
 				self.x = fx
 		elif self.d == "right" :
 			fx = self.x + fv
-			if fx > 100 : # changement de direction lorsque Schro. arrive en bout de ligne (par la droite)
-				fx = 100
-				fd = "up"
+			if fx >= 100 : # changement de direction lorsque Schro. arrive en bout de ligne (par la droite)
+				self.x = 100
+				self.d = "up"
 			else :
 				self.x = fx
 		elif self.d == "up" :
 			fy = self.y + fv
 		else :
 			print("error")
-		print(self.x)
-		print(self.y)
+		self.v = fv
+		print(self.x, self.y) # affichage pour test
+		print(self.d, self.a, self.v) # affichage pour test
