6249aebeb876011347aea47a2b94faea61728de5
Choumat <Choumat@users.noreply.github.com>
2014-03-29 14:00:55 +0100

    Update entity.py
    Suppression de variables temporaires inutiles

--- entity.py
+++ entity.py
@@ -15,28 +15,27 @@ class Entity:
 		# d : direction actuelle (haut, gauche, droite)
 		# a : accélération
 		# v : vitesse actuelle
-		fv = (1 + self.a ) * self.v
-		fx = self.x
-		fy = self.y
+		self.v = (1 + self.a ) * self.v
 
 		if self.d == "left" :
-			fx = self.x - fv
-			if fx <= 0 : # changement de direction lorsque Schro. arrive en bout de ligne (par la gauche)
+			tx = self.x - tv
+			if tx <= 0 : # changement de direction lorsque Schro. arrive en bout de ligne (par la gauche)
 				self.x = 0
 				self.d = "up"
 			else :
-				self.x = fx
+				self.x = tx
 		elif self.d == "right" :
-			fx = self.x + fv
-			if fx >= 100 : # changement de direction lorsque Schro. arrive en bout de ligne (par la droite)
+			tx = self.x + tv
+			if tx >= 100 : # changement de direction lorsque Schro. arrive en bout de ligne (par la droite)
 				self.x = 100
 				self.d = "up"
 			else :
-				self.x = fx
+				self.x = tx
 		elif self.d == "up" :
-			fy = self.y + fv
+			ty = self.y + tv
 		else :
 			print("error")
-		self.v = fv
 		print(self.x, self.y) # affichage pour test
 		print(self.d, self.a, self.v) # affichage pour test
+
+
