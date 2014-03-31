2cff7be86dce132b8bd2517e300704ccac3eca68
Choumat <Choumat@users.noreply.github.com>
2014-03-31 07:33:28 +0200

    Update test_entity.py

--- test_entity.py
+++ test_entity.py
@@ -13,7 +13,7 @@ while Schro.d == "right":
     # teste le nombre d'itérations pour que Schro. arrive à la fin de la ligne (x=100) et change de direction
     Schro.move()
     print(Schro.x, Schro.y) # affichage pour test
-	print(Schro.d, Schro.a, Schro.v) # affichage pour test
+    print(Schro.d, Schro.a, Schro.v) # affichage pour test
     n += 1
 print(n) # retourne le nombre d'itérations
 
