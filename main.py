a59e1ea1063ff37107a3338fd9680d40b49ee651
pascalcpp <pycpp@mailoo.org>
2014-05-23 09:24:47 +0200

    music

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ W, H         = 840, 840
 # Play music
 pygame.mixer.music.load( os.path.join("sound", "music.ogg") )
 pygame.mixer.music.set_volume(0.2)
-# pygame.mixer.music.play(-1) # play forever
+pygame.mixer.music.play(-1) # play forever
 
 class App :
 
